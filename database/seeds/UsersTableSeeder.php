3afa8ff29b1283673aee2aa98acd64d7f2dd2459
Eduardo Silva <eduardo@dudstecnologia.com.br>
2020-06-11 18:23:09 -0400

    Seeder Usuário

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('admin'),
+            'admin' => true
+        ]);
+    }
+}
